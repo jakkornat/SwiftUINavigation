d3fdd704cf070cb36ddc4fd318c50d2c484b64f9
Jakub Kornatowski <jakub.kornatowski@allegro.pl>
2021-04-11 09:54:51 +0200

    Initial Commit

---
+++ SwiftUINavigation/SwiftUINavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUINavigationApp.swift
+//  SwiftUINavigation
+//
+//  Created by Jakub Kornatowski on 11/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUINavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
